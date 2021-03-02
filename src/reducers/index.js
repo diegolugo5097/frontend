745da7126ec6ceb3abd427c27099b2cb4c2a071a
Stiven <pivenwar@gmail.com>
2021-03-01 19:11:48 -0500

    index del reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 import products from "./products";
 import categories from "./categories";
 import abouts from "./aboutUs";
-
-export const reducers = combineReducers({ products, categories, abouts });
+import services from "./services";
+import users from "./users";
+export const reducers = combineReducers({ products, categories, abouts, services, users });
